b5252c45bf764fcc63201c617fa5019dcd711052
yujinago <138287741+yujinago@users.noreply.github.com>
2023-07-27 00:41:55 +0000

    [Finish] アルゴリズム-6

--- air_conditioner.rb
+++ air_conditioner.rb
@@ -2,6 +2,7 @@ input_lines = readlines
 
 room = input_lines[0].to_i
 air_conditioner = input_lines[1].to_i
+air_flow = input_lines[2].to_i
 
 temperature_diff = (room - air_conditioner).abs
 
@@ -13,6 +14,17 @@ elsif temperature_diff >=5 && temperature_diff < 10 then
     require_time = 30
 elsif temperature_diff >= 10 then
     require_time = 60
+end
+
+if air_flow == 1
+    require_time
+elsif air_flow == 2
+    require_time = require_time - 5
+elsif air_flow == 3
+    require_time = require_time - 10
+else
+    p "風量の値は1~3にしてください"
+    exit
 end
 
 p require_time
